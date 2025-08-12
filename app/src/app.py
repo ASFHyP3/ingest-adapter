322efdd53356eaa09245e458fa1a44148201c5e2
Jake Herrmann <jtherrmann@alaska.edu>
2025-08-11 16:05:40 -0800

    move ARIA_S1_GUNW code into separate module

--- app/src/app.py
+++ app/src/app.py
@@ -1,64 +1,11 @@
-import datetime
 import json
 import os
-import pathlib
 import traceback
 
 import boto3
 import hyp3_sdk
-import requests
 
-
-def get_granule_ur_pattern(granule_ur: str) -> str:
-    return granule_ur.rsplit('-', 1)[0] + '-*'
-
-
-def exists_in_cmr(cmr_domain: str, granule_ur: str) -> bool:
-    url = f'https://{cmr_domain}/search/granules.umm_json'
-    params = (
-        ('short_name', 'ARIA_S1_GUNW'),
-        ('granule_ur', get_granule_ur_pattern(granule_ur)),
-        ('options[granule_ur][pattern]', 'true'),
-        ('page_size', 1),
-    )
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    if response.json()['items']:
-        print(f'{granule_ur} already exists in CMR as {response.json()["items"][0]["umm"]["GranuleUR"]}')
-        return True
-    return False
-
-
-def generate_ingest_message(hyp3_job_dict: dict) -> dict:
-    bucket = hyp3_job_dict['files'][0]['s3']['bucket']
-    product_key = pathlib.Path(hyp3_job_dict['files'][0]['s3']['key'])
-
-    return {
-        'ProductName': product_key.stem,
-        'DeliveryTime': datetime.datetime.now(tz=datetime.UTC).replace(tzinfo=None).isoformat(),
-        'Browse': {
-            'Bucket': bucket,
-            'Key': str(product_key.with_suffix('.png')),
-        },
-        'Metadata': {
-            'Bucket': bucket,
-            'Key': str(product_key.with_suffix('.json')),
-        },
-        'Product': {
-            'Bucket': bucket,
-            'Key': str(product_key),
-        },
-    }
-
-
-def publish_message(message: dict, topic_arn: str) -> None:
-    print(f'Publishing {message["ProductName"]} to {topic_arn}')
-    topic_region = topic_arn.split(':')[3]
-    sns = boto3.client('sns', region_name=topic_region)
-    sns.publish(
-        TopicArn=topic_arn,
-        Message=json.dumps(message),
-    )
+import aria_s1_gunw
 
 
 def get_job_dict(hyp3_url: str, username: str, password: str, job_id: str) -> dict:
@@ -71,11 +18,11 @@ def process_message(message: dict, edl_credentials: dict) -> None:
     username, password = edl_credentials['username'], edl_credentials['password']
     job = get_job_dict(message['hyp3_url'], username, password, message['job_id'])
 
-    if job['job_type'] != 'ARIA_S1_GUNW':
-        raise ValueError(f'Job type {job["job_type"]} is not supported; must be ARIA_S1_GUNW')
-    ingest_message = generate_ingest_message(job)
-    if not exists_in_cmr(os.environ['CMR_DOMAIN'], ingest_message['ProductName']):
-        publish_message(ingest_message, os.environ['INGEST_TOPIC_ARN'])
+    match job['job_type']:
+        case 'ARIA_S1_GUNW':
+            aria_s1_gunw.process_aria_s1_gunw(job)
+        case _:
+            raise ValueError(f'Job type {job["job_type"]} is not supported')
 
 
 def load_credentials() -> dict:
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,130 +4,8 @@ from unittest.mock import MagicMock, call, patch
 
 import hyp3_sdk
 import pytest
-import responses
 
 import app
-
-
-def test_get_granule_ur_pattern():
-    granule_ur = 'S1-GUNW-D-R-036-tops-20250131_20241226-041630-00025E_00035N-PP-99eb-v3_0_1'
-    expected = 'S1-GUNW-D-R-036-tops-20250131_20241226-041630-00025E_00035N-PP-99eb-*'
-    assert app.get_granule_ur_pattern(granule_ur) == expected
-
-    granule_ur = 'S1-GUNW-D-R-123-tops-20230605_20230512-032645-00038E_00036N-PP-f518-v3_0_0'
-    expected = 'S1-GUNW-D-R-123-tops-20230605_20230512-032645-00038E_00036N-PP-f518-*'
-    assert app.get_granule_ur_pattern(granule_ur) == expected
-
-
-@responses.activate
-def test_exists_in_cmr():
-    responses.get(
-        'https://cmr.earthdata.nasa.gov/search/granules.umm_json',
-        status=200,
-        match=[
-            responses.matchers.query_param_matcher(
-                {
-                    'short_name': 'ARIA_S1_GUNW',
-                    'granule_ur': 'S1-GUNW-D-R-036-tops-20250131_20241226-041630-00025E_00035N-PP-99eb-*',
-                    'options[granule_ur][pattern]': 'true',
-                    'page_size': 1,
-                },
-            ),
-        ],
-        json={
-            'items': [
-                {
-                    'umm': {
-                        'GranuleUR': 'myGranule',
-                    },
-                },
-            ],
-        },
-    )
-    granule_ur = 'S1-GUNW-D-R-036-tops-20250131_20241226-041630-00025E_00035N-PP-99eb-v3_0_1'
-    assert app.exists_in_cmr('cmr.earthdata.nasa.gov', granule_ur)
-
-    responses.get(
-        'https://cmr.uat.earthdata.nasa.gov/search/granules.umm_json',
-        status=200,
-        match=[
-            responses.matchers.query_param_matcher(
-                {
-                    'short_name': 'ARIA_S1_GUNW',
-                    'granule_ur': 'S1-GUNW-D-R-123-tops-20230605_20230512-032645-00038E_00036N-PP-f518-*',
-                    'options[granule_ur][pattern]': 'true',
-                    'page_size': 1,
-                },
-            ),
-        ],
-        json={
-            'items': [],
-        },
-    )
-    granule_ur = 'S1-GUNW-D-R-123-tops-20230605_20230512-032645-00038E_00036N-PP-f518-v3_0_0'
-    assert not app.exists_in_cmr('cmr.uat.earthdata.nasa.gov', granule_ur)
-
-
-def test_generate_ingest_message(monkeypatch):
-    job = {
-        'files': [
-            {
-                's3': {
-                    'bucket': 'myBucket',
-                    'key': 'myPrefix/myFilename.nc',
-                },
-            },
-        ],
-    }
-    expected = {
-        'ProductName': 'myFilename',
-        'DeliveryTime': '2025-02-18T01:02:03.000456',
-        'Browse': {
-            'Bucket': 'myBucket',
-            'Key': 'myPrefix/myFilename.png',
-        },
-        'Metadata': {
-            'Bucket': 'myBucket',
-            'Key': 'myPrefix/myFilename.json',
-        },
-        'Product': {
-            'Bucket': 'myBucket',
-            'Key': 'myPrefix/myFilename.nc',
-        },
-    }
-
-    now = datetime.datetime(2025, 2, 18, 1, 2, 3, 456)
-    mock_datetime = MagicMock(wraps=datetime.datetime)
-    mock_datetime.now.return_value = now
-    monkeypatch.setattr(datetime, 'datetime', mock_datetime)
-
-    assert app.generate_ingest_message(job) == expected
-
-
-def test_publish_message():
-    with patch('boto3.client') as mock_client:
-        mock_sns = MagicMock()
-        mock_client.return_value = mock_sns
-
-        app.publish_message({'ProductName': 'foo'}, 'arn:aws:sns:us-east-1:123456789012:myTopic')
-
-        mock_client.assert_called_once_with('sns', region_name='us-east-1')
-        mock_sns.publish.assert_called_once_with(
-            TopicArn='arn:aws:sns:us-east-1:123456789012:myTopic',
-            Message='{"ProductName": "foo"}',
-        )
-
-    with patch('boto3.client') as mock_client:
-        mock_sns = MagicMock()
-        mock_client.return_value = mock_sns
-
-        app.publish_message({'ProductName': 'bar'}, 'arn:aws:sns:us-west-2:123456789012:myTopic')
-
-        mock_client.assert_called_once_with('sns', region_name='us-west-2')
-        mock_sns.publish.assert_called_once_with(
-            TopicArn='arn:aws:sns:us-west-2:123456789012:myTopic',
-            Message='{"ProductName": "bar"}',
-        )
 
 
 def test_get_job_dict():
@@ -157,6 +35,7 @@ def test_get_job_dict():
         mock_hyp3.get_job_by_id.assert_called_once_with('abc123')
 
 
+# TODO: contains logic specific to ARIA_S1_GUNW; mock out process_aria_s1_gunw and test it separately?
 def test_process_message(monkeypatch):
     monkeypatch.setenv('CMR_DOMAIN', 'cmr.earthdata.nasa.gov')
     monkeypatch.setenv('INGEST_TOPIC_ARN', 'myTopicArn')
@@ -167,43 +46,47 @@ def test_process_message(monkeypatch):
 
     with (
         patch('app.get_job_dict', return_value={'job_type': 'ARIA_S1_GUNW'}) as get_job_dict,
-        patch('app.generate_ingest_message', return_value={'ProductName': 'foo'}) as generate_ingest_message,
-        patch('app.exists_in_cmr', return_value=False) as exists_in_cmr,
-        patch('app.publish_message') as publish_message,
+        patch(
+            'aria_s1_gunw._generate_ingest_message', return_value={'ProductName': 'foo'}
+        ) as mock_generate_ingest_message,
+        patch('aria_s1_gunw._exists_in_cmr', return_value=False) as mock_exists_in_cmr,
+        patch('aria_s1_gunw._publish_message') as mock_publish_message,
     ):
         app.process_message({'hyp3_url': 'https://foo.com', 'job_id': 'abc123'}, credentials)
 
         get_job_dict.assert_called_once_with('https://foo.com', 'myUsername', 'myPassword', 'abc123')
-        generate_ingest_message.assert_called_once_with({'job_type': 'ARIA_S1_GUNW'})
-        exists_in_cmr.assert_called_once_with('cmr.earthdata.nasa.gov', 'foo')
-        publish_message.assert_called_once_with({'ProductName': 'foo'}, 'myTopicArn')
+        mock_generate_ingest_message.assert_called_once_with({'job_type': 'ARIA_S1_GUNW'})
+        mock_exists_in_cmr.assert_called_once_with('cmr.earthdata.nasa.gov', 'foo')
+        mock_publish_message.assert_called_once_with({'ProductName': 'foo'}, 'myTopicArn')
 
     with (
         patch('app.get_job_dict', return_value={'job_type': 'ARIA_S1_GUNW'}) as get_job_dict,
-        patch('app.generate_ingest_message', return_value={'ProductName': 'bar'}) as generate_ingest_message,
-        patch('app.exists_in_cmr', return_value=True) as exists_in_cmr,
-        patch('app.publish_message') as publish_message,
+        patch(
+            'aria_s1_gunw._generate_ingest_message', return_value={'ProductName': 'bar'}
+        ) as mock_generate_ingest_message,
+        patch('aria_s1_gunw._exists_in_cmr', return_value=True) as mock_exists_in_cmr,
+        patch('aria_s1_gunw._publish_message') as mock_publish_message,
     ):
         app.process_message({'hyp3_url': 'https://bar.com', 'job_id': 'def456'}, credentials)
 
         get_job_dict.assert_called_once_with('https://bar.com', 'myUsername', 'myPassword', 'def456')
-        generate_ingest_message.assert_called_once_with({'job_type': 'ARIA_S1_GUNW'})
-        exists_in_cmr.assert_called_once_with('cmr.earthdata.nasa.gov', 'bar')
-        publish_message.assert_not_called()
+        mock_generate_ingest_message.assert_called_once_with({'job_type': 'ARIA_S1_GUNW'})
+        mock_exists_in_cmr.assert_called_once_with('cmr.earthdata.nasa.gov', 'bar')
+        mock_publish_message.assert_not_called()
 
     with (
         patch('app.get_job_dict', return_value={'job_type': 'BAD_JOB_TYPE'}) as get_job_dict,
-        patch('app.generate_ingest_message') as generate_ingest_message,
-        patch('app.exists_in_cmr') as exists_in_cmr,
-        patch('app.publish_message') as publish_message,
+        patch('aria_s1_gunw._generate_ingest_message') as mock_generate_ingest_message,
+        patch('aria_s1_gunw._exists_in_cmr') as mock_exists_in_cmr,
+        patch('aria_s1_gunw._publish_message') as mock_publish_message,
     ):
-        with pytest.raises(ValueError, match=r'^Job type BAD_JOB_TYPE is not supported.*'):
+        with pytest.raises(ValueError, match=r'^Job type BAD_JOB_TYPE is not supported$'):
             app.process_message({'hyp3_url': 'https://bar.com', 'job_id': 'def456'}, credentials)
 
         get_job_dict.assert_called_once_with('https://bar.com', 'myUsername', 'myPassword', 'def456')
-        generate_ingest_message.assert_not_called()
-        exists_in_cmr.assert_not_called()
-        publish_message.assert_not_called()
+        mock_generate_ingest_message.assert_not_called()
+        mock_exists_in_cmr.assert_not_called()
+        mock_publish_message.assert_not_called()
 
 
 def test_load_credentials(monkeypatch):
@@ -240,7 +123,7 @@ def test_lambda_handler():
                 call({'hyp3_url': 'url2', 'job_id': 'id2'}, credentials),
             ],
         )
-        mock_load_credentials.assert_called_once()
+        mock_load_credentials.assert_called_once_with()
 
     event = {
         'Records': [
@@ -249,3 +132,4 @@ def test_lambda_handler():
     }
     with patch('app.load_credentials', return_value=credentials) as mock_load_credentials:
         assert app.lambda_handler(event, None) == {'batchItemFailures': [{'itemIdentifier': 'myMessageId'}]}
+        mock_load_credentials.assert_called_once_with()
