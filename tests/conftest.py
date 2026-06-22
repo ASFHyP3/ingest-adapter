eccb5a9d5ef60f02fe1aff8eca609e83419f07cf
William Horn <wbhorn@alaska.edu>
2026-06-22 14:27:46 -0800

    fix: Add more env variables to dummy aws config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,19 @@
 import pathlib
+import os
 
 import pytest
 from moto import mock_aws
 from moto.core import patch_client
+
+
+def pytest_configure(config):
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    os.environ['AWS_DEFAULT_REGION'] = 'us-west-2'
+    os.environ['AWS_REGION'] = 'us-west-2'
 
 
 @pytest.fixture
